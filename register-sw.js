fa8a92ae8c06d3a6b18fa536ee208930d69d4d7a
Anik Chowdhury <58397348+anikchowdhurybd@users.noreply.github.com>
2025-05-22 15:07:04 +0600

    addedd

--- register-sw.js
+++ register-sw.js
@@ -1,1 +1,17 @@
- 
+<script type='text/javascript'>
+//<![CDATA[
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }) // scope: '/' is crucial
+      .then(function(registration) {
+        console.log('PWA Service Worker: Registration successful for schemamarkupgenerator.net with scope: ', registration.scope);
+      })
+      .catch(function(err) {
+        console.log('PWA Service Worker: Registration failed for schemamarkupgenerator.net: ', err);
+      });
+  });
+} else {
+  console.log('PWA Service Worker: Not supported in this browser for schemamarkupgenerator.net.');
+}
+//]]>
+</script>
